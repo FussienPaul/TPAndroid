fb5a278b202acf7102e94e520a200b9dec23324c
Fussien <Fussienpaul@hotmail.fr>
2019-11-07 06:49:25 -0500

    AFFICHAGE PHOTOS DIFERENTES

--- GestureExampleActivity2/app/src/main/java/com/example/bamenela/gestureexampleactivity/TouchExample.java
+++ GestureExampleActivity2/app/src/main/java/com/example/bamenela/gestureexampleactivity/TouchExample.java
@@ -9,6 +9,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -19,22 +20,30 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.LogRecord;
 
 
 public class TouchExample extends View {
     private static final int MAX_POINTERS = 5;
+    private Canvas canvas;
     final BitmapFactory.Options options = new BitmapFactory.Options();
     private float mScale = 1f;
-    private int currentNbColumn = 7;
+    private int currentNbColumn = 3;
+    private int height = getResources().getDisplayMetrics().heightPixels;
+    private int width = getResources().getDisplayMetrics().widthPixels;
     private int bitmapResolution = getBitMapResolution(currentNbColumn, "dpi");
     //private int maxBitmapRow = getBitmapRow(bitmapResolution);
     private GestureDetector mGestureDetector;
+    int load=0;
+    private Handler objectHandler;
     private ScaleGestureDetector mScaleGestureDetector;
     private ArrayList<String> images;
     private HashMap<Integer, BitmapDrawable> imageList = new HashMap<Integer, BitmapDrawable>();
     private Pointer[] mPointers = new Pointer[MAX_POINTERS];
     private Paint mPaint;
     private float mFontSize;
+    private int index=0;
+    private float touchPositionY=0,mouvement=0;
 
     class Pointer {
         float x = 0;
@@ -56,21 +65,72 @@ public class TouchExample extends View {
 
         mGestureDetector = new GestureDetector(context, new ZoomGesture());
         mScaleGestureDetector = new ScaleGestureDetector(context, new ScaleGesture());
+        //loadWithThreads(Singleton.getInstance().listImageMemory.size());
 
 //        Toast.makeText(getContext(), "Row: "+maxBitmapRow, Toast.LENGTH_SHORT).show();
 
+    }
+/*
+    public void loadWithThreads(final int Maxindex){
+        load = 0;
+        final Handler handlerObject = new Handler();
+        handlerObject.post(new Runnable() {
+            @Override
+            public void run() {
+                if (load < Singleton.getInstance().listImageMemory.size()) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(Singleton.getInstance().listImageMemory.get(load), options);
+                    Log.d("LOADTREAD", "Thread PRE loaded load ID------------------- :"+load);
+                    imageList.put(load, new BitmapDrawable(getResources(), bitmap));
+                    load++;
+                    if(load<Maxindex)
+                    {
+                        handlerObject.post(this);
+                    }
+                }
+            }
+        });
+    }
+*/
+    public void dispPicture(int index, int x, int y,int xmax, int ymax, Canvas canvas)
+    {
+            Bitmap bitmap = BitmapFactory.decodeFile(Singleton.getInstance().listImageMemory.get(index), options);
+            imageList.put(index,new BitmapDrawable(getResources(), bitmap));
+        BitmapDrawable image = imageList.get(index);
+
+        image.setBounds(x,y,xmax,ymax);
+        Log.d("LOADTREAD", "Thread PRE loaded ID------------------- :"+index);
+        image.draw(canvas);
+    }
+
+    public void findPosition(int currentNbColumn){
+        int posx=0, posy=0, posxmax=0, posymax=0;
+        int nbligne= height/(width/currentNbColumn);
+        for(int j=0;j<nbligne;j++){
+            for(int i=0;i<currentNbColumn;i++) {
+                posx = i * (width / currentNbColumn);
+                posxmax = (i+1) * (width / currentNbColumn);
+                posy = j * (width / currentNbColumn);
+                posymax = (j+1) * (width / currentNbColumn);
+                dispPicture(index,posx,posy,posxmax,posymax,canvas);
+                if(index<Singleton.getInstance().listImageMemory.size())
+                {
+                    index++;
+                }
+            }
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap bitmap = BitmapFactory.decodeFile(Singleton.getInstance().listImageMemory.get(0), options);
-        imageList.put(0,new BitmapDrawable(getResources(), bitmap));
-        BitmapDrawable image = imageList.get(0);
-        image.draw(canvas);
-
-
-        canvas.drawBitmap(image.getBitmap(), 0, 0, mPaint);
+        this.canvas=canvas;
+        //Bitmap bitmap = BitmapFactory.decodeFile(Singleton.getInstance().listImageMemory.get(0), options);
+        //imageList.put(0,new BitmapDrawable(getResources(), bitmap));
+        //BitmapDrawable image = imageList.get(0);
+        //image.draw(canvas);
+        findPosition(currentNbColumn);
+        //loadWithThreads(Singleton.getInstance().listImageMemory.size());
+        //canvas.drawBitmap(image.getBitmap(), 0, 0, mPaint);
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -121,6 +181,26 @@ public class TouchExample extends View {
             invalidate();
             bitmapResolution = getBitMapResolution((normal ? 7 : 3),"dpi");
             //maxBitmapRow = getBitmapRow(bitmapResolution);
+            return true;
+        }
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY){
+            mouvement = touchPositionY + distanceY;
+            if(mouvement>400){
+                Log.d("MOVE", "MOUVE DOWN ");
+                touchPositionY  = distanceY;
+                index = index+currentNbColumn;
+                mouvement =0;
+            }
+            if(mouvement<-400){
+                Log.d("MOVE", "MOUVE UP ");
+                touchPositionY  = distanceY;
+
+                index = index-currentNbColumn;
+                mouvement =0;
+            }
+            Log.d("MOVE", "Y : "+distanceY +" mouv " +mouvement+"index = "+index);
+            touchPositionY  = distanceY;
             return true;
         }
     }
